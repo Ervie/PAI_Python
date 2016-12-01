32a8b00c13574212d936605c424babfd71f157a4
Bartłomiej Buchała <bbuchala93@gmail.com>
2016-12-01 23:22:02 +0100

    Player już stoi.
    Tylko póki co bylejak i z 1 stacją zahardkodowaną.

---
+++ RadioStreamer/RadioStreamer/app/static/app/scripts/player/set.player.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+
+    var stream = {
+        title: "Touhou Radio",
+        mp3: "http://37.59.41.178:8000/;stream/1"
+    },
+            ready = false;
+
+    $("#jquery_jplayer_1").jPlayer({
+        ready: function (event) {
+            ready = true;
+            $(this).jPlayer("setMedia", stream);
+        },
+        pause: function () {
+            $(this).jPlayer("clearMedia");
+        },
+        error: function (event) {
+            if (ready && event.jPlayer.error.type === $.jPlayer.error.URL_NOT_SET) {
+                // Setup the media stream again and play it.
+                $(this).jPlayer("setMedia", stream).jPlayer("play");
+            }
+        },
+        cssSelectorAncestor: "#jp_container_1",
+        swfPath: "../player",
+        solution: 'html, flash',
+        supplied: "mp3",
+        useStateClassSkin: true,
+        autoBlur: false,
+        smoothPlayBar: true,
+        keyEnabled: true
+    });
+});
