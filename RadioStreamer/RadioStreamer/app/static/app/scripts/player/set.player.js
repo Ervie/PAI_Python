35789a5b6bf8978ad5a0672f85611c0a565be57e
Ervin <bbuchala93@gmail.com>
2016-12-02 13:02:43 +0100

    Przycisk do losowania stacji

--- RadioStreamer/RadioStreamer/app/static/app/scripts/player/set.player.js
+++ RadioStreamer/RadioStreamer/app/static/app/scripts/player/set.player.js
@@ -23,10 +23,40 @@
         cssSelectorAncestor: "#jp_container_1",
         swfPath: "../player",
         solution: 'html, flash',
-        supplied: "mp3",
+        supplied: "mp3, oga",
         useStateClassSkin: true,
         autoBlur: false,
         smoothPlayBar: true,
         keyEnabled: true
     });
+});
+
+$(document).ready(function () {
+	$("#ChangeChannelBtn").click(function () {
+
+		RandomNumber = Math.random();
+
+		if (RandomNumber > 0.66) {
+			var stream = {
+				title: "RMF Classic",
+				mp3: "http://195.150.20.243:8000/rmf_classic"
+			}
+		} else if (RandomNumber > 0.33) {
+			var stream = {
+				title: "Touhou Radio",
+				mp3: "http://37.59.41.178:8000/;stream/1"
+			}
+		} else {
+			var stream = {
+				title: "RMF Game Music",
+				mp3: "http://185.69.192.87/GAMEMUSIC"
+			}
+		}
+
+		
+
+		
+		$('#jquery_jplayer_1').jPlayer('setMedia', stream);
+		
+	});
 });
